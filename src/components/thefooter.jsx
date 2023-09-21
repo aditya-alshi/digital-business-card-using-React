1a76f39428ac0a26501d5fe77c3c445c5e4a0da9
Aditya Alshi <alshiaditya55@gmail.com>
2023-09-21 13:41:29 +0530

    shifted to public

--- src/components/thefooter.jsx
+++ src/components/thefooter.jsx
@@ -5,16 +5,16 @@ export default function Footer() {
     <>
       <div className="social-media-links compo">
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <img src="/src/assets/twitter-Icon.png" alt="Twitter" className="all-social-media-icons" />
+          <img src="/public/assets/twitter-Icon.png" alt="Twitter" className="all-social-media-icons" />
         </a>
         <a href="https://www.facebook.com/aditya.alshi.73" target="_blank" rel="noopener noreferrer">
-          <img src="/src/assets/facebook -Icon.png" alt="Facebook" className="all-social-media-icons" />
+          <img src="/public/assets/facebook -Icon.png" alt="Facebook" className="all-social-media-icons" />
         </a>
         <a href="https://www.instagram.com/aditya_alshi_/" target="_blank" rel="noopener noreferrer">
-          <img src="/src/assets/instagram-Icon.png" alt="Instagram" className="all-social-media-icons" />
+          <img src="/public/assets/instagram-Icon.png" alt="Instagram" className="all-social-media-icons" />
         </a>
         <a href="https://github.com/aditya-alshi" target="_blank" rel="noopener noreferrer">
-          <img src="/src/assets/github-Icon.png" alt="GitHub" className="all-social-media-icons" />
+          <img src="/public/assets/github-Icon.png" alt="GitHub" className="all-social-media-icons" />
         </a>
       </div>
     </>
