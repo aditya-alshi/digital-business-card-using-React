8cd150f9d1ce234cf3092114ff94bb196ed553f7
Aditya Alshi <alshiaditya55@gmail.com>
2023-09-27 08:37:15 +0530

    udated path post experiments

--- src/components/thefooter.jsx
+++ src/components/thefooter.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
     <>
       <div className="social-media-links compo">
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <img src="/twitter-Icon.png" alt="Twitter" className="all-social-media-icons" />
+          <img src="twitter-Icon.png" alt="Twitter" className="all-social-media-icons" />
         </a>
         <a href="https://www.facebook.com/aditya.alshi.73" target="_blank" rel="noopener noreferrer">
-          <img src="/facebook -Icon.png" alt="Facebook" className="all-social-media-icons" />
+          <img src="facebook -Icon.png" alt="Facebook" className="all-social-media-icons" />
         </a>
         <a href="https://www.instagram.com/aditya_alshi_/" target="_blank" rel="noopener noreferrer">
           <img src="instagram-Icon.png" alt="Instagram" className="all-social-media-icons" />
